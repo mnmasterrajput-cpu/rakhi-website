fdbea2c613f4944656b1584bfef689353d624d3a
mnmasterrajput-cpu <mnmasterrajput@gmail.com>
2026-08-26 08:10:06 +0530

    Change MySQL connection to use connection pool

--- backend/dbconnection/db.js
+++ backend/dbconnection/db.js
@@ -1,22 +1,29 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if (err) {
         console.log("MySQL connection failed:", err);
-    } else {
-        console.log("MySQL connected successfully");
+        return;
     }
+
+    console.log("MySQL connected successfully");
+    connection.release();
 });
 
 module.exports = db;
